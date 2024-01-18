d2f23db6d4f4274f2bda270877e4833a1119978c
Diego Coira <dcoirap@fpcoruna.afundacion.org>
2024-01-18 12:11:14 +0100

    Login and register added, and update in models

--- project/backend/licopedia/models.py
+++ project/backend/licopedia/models.py
@@ -12,17 +12,70 @@ class CustomUser(models.Model):
     address = models.CharField(max_length=255, null=True, blank=True)
     telephone = models.IntegerField(null=True, blank=True)
 
+    def to_json_edit_user(self):
+        return {
+            "username": self.username,
+            "name": self.name,
+            "surname": self.surname,
+            "email": self.email,
+            "password": self.password
+        }
+
+    def to_json_personal_data(self):
+        return {
+            "email": self.email,
+            "password": self.password,
+            "address": self.address,
+            "telephone": self.telephone
+        }
+
+    def to_json_all(self):
+        return {
+            "username": self.username,
+            "name": self.name,
+            "surname": self.surname,
+            "email": self.email,
+            "password": self.password,
+            "address": self.address,
+            "telephone": self.telephone
+        }
+
+
+class UserSession(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    token = models.CharField(unique=True, max_length=45)
+
 
 class Soda(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     image = models.URLField()
 
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "image": self.image,
+        }
+
 
 class Liquor(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     image = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "image": self.image,
+        }
 
 
 class Cocktails(models.Model):
@@ -31,3 +84,20 @@ class Cocktails(models.Model):
     image = models.URLField()
     soda = models.ForeignKey(Soda, on_delete=models.CASCADE)
     liquor = models.ForeignKey(Liquor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "description": self.description,
+            "image": self.image,
+            "soda": self.soda,
+            "liquor": self.liquor
+        }
+
+
+class SavedCocktails(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    cocktail = models.ForeignKey(Cocktails, on_delete=models.CASCADE)
--- project/backend/licopedia/views.py
+++ project/backend/licopedia/views.py
@@ -1,3 +1,69 @@
+import json
+import secrets
+import bcrypt
+
+from django.views import View
+from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import *
+
 
 # Create your views here.
+
+@csrf_exempt
+def sessions(request):
+    if request.method != 'POST':
+        return JsonResponse({'error': 'Not supported HTTP method'}, status=405)
+    body_json = json.loads(request.body)
+    required_params = ['email', 'password']
+    missing_params = [param for param in required_params if param not in body_json]
+    if missing_params:
+        return JsonResponse({"error": "You are missing a parameter"}, status=400)
+    json_email = body_json['email']
+    json_password = body_json['password']
+    try:
+        db_user = CustomUser.objects.get(email=json_email)
+    except CustomUser.DoesNotExist:
+        return JsonResponse({"error": "User not in database"}, status=404)
+
+    if bcrypt.checkpw(json_password.encode('utf8'), db_user.password.encode('utf8')):
+        random_token = secrets.token_hex(10)
+        session = UserSession(user=db_user, token=random_token)
+        session.save()
+        return JsonResponse({"token": random_token}, status=201)
+    else:
+        return JsonResponse({"error": "Invalid password"}, status=401)
+
+
+@csrf_exempt
+def register(request):
+    if request.method != 'POST':
+        return JsonResponse({'error': 'Unsupported HTTP method'}, status=405)
+
+    body_json = json.loads(request.body)
+    required_params = ['name', 'email', 'password']
+
+    if not all(param in body_json for param in required_params):
+        return JsonResponse({'error': 'Missing parameter in body request'}, status=400)
+
+    json_name = body_json['name']
+    json_email = body_json['email']
+    json_password = body_json['password']
+
+    if '@' not in json_email or len(json_email) < 5:
+        return JsonResponse({'error': 'Not valid email'}, status=400)
+
+    if CustomUser.objects.filter(email=json_email).exists():
+        return JsonResponse({'error': 'User already registered.'}, status=401)
+
+    salted_and_hashed_pass = bcrypt.hashpw(json_password.encode('utf8'), bcrypt.gensalt()).decode('utf8')
+
+    user_object = CustomUser(name=json_name, email=json_email, password=salted_and_hashed_pass)
+    user_object.save()
+
+    # Generamos un token para el usuario
+    token = secrets.token_hex(16)
+    return JsonResponse({"token": token}, status=200)
