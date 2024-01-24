f8fd53205566a5aefc9e40cb2129c10aa06213c4
AndresSoto <asotol22@fpcoruna.afundacion.org>
2024-01-24 21:35:04 +0100

    Implementado el login a firebase

--- project/frontend/app/build.gradle.kts
+++ project/frontend/app/build.gradle.kts
@@ -34,8 +34,10 @@ android {
 
 dependencies {
 
-    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database:20.0.0")
     implementation("com.android.volley:volley:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -44,3 +46,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+
--- project/frontend/app/src/main/java/com/app/licopedia/sign_in.java
+++ project/frontend/app/src/main/java/com/app/licopedia/sign_in.java
@@ -7,17 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class sign_in extends AppCompatActivity {
     
     private EditText email, password;
     private String semail, spassword;
     private Button sign_in, register;
+    private FirebaseAuth auth;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+        
+        auth = FirebaseAuth.getInstance();
         
         email = findViewById(R.id.sign_in_email);
         password = findViewById(R.id.sign_in_password);
@@ -28,10 +34,21 @@ public class sign_in extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 
-                semail = email.getText().toString();
-                spassword = password.getText().toString();
+                semail = email.getText().toString().trim();
+                spassword = password.getText().toString().trim();
                 
-                
+                auth.signInWithEmailAndPassword(semail, spassword)
+                        .addOnCompleteListener(sign_in.this, task -> {
+                    
+                    if ((task.isSuccessful())){
+                        
+                        startActivity(new Intent(com.app.licopedia.sign_in.this, mainActivity.class));
+                        finish();
+                        
+                    }
+                    else
+                        Toast.makeText(com.app.licopedia.sign_in.this, "Error at Sign IN", Toast.LENGTH_LONG).show();
+                });
                 
             }
         });
@@ -41,7 +58,7 @@ public class sign_in extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 
-                //startActivity(new Intent(sign_in.this, register.class));
+                startActivity(new Intent(sign_in.this, register.class));
                 
             }
         });
