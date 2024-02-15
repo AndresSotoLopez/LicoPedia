a332ab509e9d27193ec869e7ba5c71c2fc95901e
AndresSoto <asotol22@fpcoruna.afundacion.org>
2024-02-15 16:14:57 +0100

    Change name to the xml

--- project/frontend/app/src/main/java/com/app/licopedia/pantalla_preparacion.java
+++ project/frontend/app/src/main/java/com/app/licopedia/pantalla_preparacion.java
@@ -9,6 +9,6 @@ public class pantalla_preparacion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pantalla_preparacion);
     }
 }
