4bc63c87f06319400a59d00a3418ec438d7b5721
Alica bogarin <abogarinv@fpcoruna.afundacion.org>
2024-02-19 22:19:49 +0100

    Mejoras en camino

--- project/frontend/app/src/main/java/com/app/licopedia/Licor_Seleccionado.java
+++ project/frontend/app/src/main/java/com/app/licopedia/Licor_Seleccionado.java
@@ -1,5 +1,6 @@
 package com.app.licopedia;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,6 +11,12 @@ public class Licor_Seleccionado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_licor_seleccionado);
+
+
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
 
     }
 }
