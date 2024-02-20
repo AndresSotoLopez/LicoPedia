322904669c67ef79a4f903f273b848aa89c50f5f
Alicia <abogarinv@afundacionfpcoruna.org>
2024-02-20 10:13:32 +0100

    Licor_Seleccionado,listo para mergear

--- project/frontend/app/build.gradle.kts
+++ project/frontend/app/build.gradle.kts
@@ -32,7 +32,9 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.github.bumptech.glide:glide:4.13.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.android.volley:volley:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
