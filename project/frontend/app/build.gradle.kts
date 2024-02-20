09ebd44032c35e96b2eeecbcf34ce88fd864cef9
RaulBreaFernandez <145338589+RaulBreaFernandez@users.noreply.github.com>
2024-02-20 09:57:57 +0100

    Rama mapa de bares completada

--- project/frontend/app/build.gradle.kts
+++ project/frontend/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -29,10 +30,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.android.volley:volley:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
